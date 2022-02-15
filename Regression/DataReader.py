99dadb1be410c98db77f183a811ab0ccd78d0f46
connlost <hi.imhpc@outlook.com>
2022-02-14 21:36:56 -0600

    univariable

---
+++ Regression/DataReader.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# class DataReader(object):
+#     def __init__(self):
+#         pass
+#
+#     def get_instance(self, path: str):
+#         pass
+
+
+# data["Cement (component 1)(kg in a m^3 mixture)"]
+pass
